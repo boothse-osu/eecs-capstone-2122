a55101bbd5515017d88db97ff0c58bc094117a57
Lewis0305 <lewiss4@oregonstate.edu>
2022-04-27 16:43:50 -0700

    Added Homing

--- arduino/firmware/printer_control.cpp
+++ arduino/firmware/printer_control.cpp
@@ -1,19 +1,22 @@
 #include "Arduino.h"
 
-#include "SPI.h"
-#include "AMIS30543.h"
+//#include "SPI.h"
+//#include "AMIS30543.h"
 
 #include "usb_lib.h"
 #include "printer_control.h"
 
 float time_steps [5];
 
-void move_command(int stp_cnt[5], bool ht_nd){
+
+bool move_command(int stp_cnt[5], bool ht_nd){
+    struct VoltageAverage voltage_log[MTR_NUMBER];
+    for(int i=0; i<MTR_NUMBER; i++) voltage_log[i] = createVoltageAverage();
     unsigned long start_time = millis();
 
     for(int i = 0; i<5; i++) {
-      if(stp_cnt[i] >= 0) setDirection(i,0);
-      else setDirection(i,1);
+      //if(stp_cnt[i] >= 0) setDirection(i,0);
+      //else setDirection(i,1);
     }
   
     int steps_taken[5] = {0, 0, 0, 0, 0};
@@ -30,16 +33,25 @@ void move_command(int stp_cnt[5], bool ht_nd){
     unsigned long timeEnd = micros() + time_per_cm;
     unsigned long timeNow;
     unsigned long timeTotal;
-    
+    unsigned int stall_check = 0;
     while(true) {
         timeTotal = micros();
+        stall_check++;
+        if(stall_check==Output_Time) {
+          for(int i = 0; i<5; i++) {
+            if(pushRollingAverage(random(70), &voltage_log[i]) == false) {
+              stop_message("Stall on motor " + String(i));
+              return false;
+            }
+          }
+        }
         for(int i = 0; i<5; i++) {
             timeNow = micros();
             //Serial.println(String((timeNow-time_nxt_step[i])/time_steps[i]));
             for(int k = 0; k<(timeNow-time_nxt_step[i])/time_steps[i]; k++){
                 //send_message("Step mtr " + String(i));
-  
-                step(i);
+
+                //step(i);
                 time_nxt_step[i] += time_steps[i];
                 steps_taken[i]+= stp_cnt[i] / abs(stp_cnt[i]);
                 if(steps_taken[i]== stp_cnt[i]) break;
@@ -51,8 +63,31 @@ void move_command(int stp_cnt[5], bool ht_nd){
     for(int i = 0; i<5; i++) send_message("MTR "+String(i)+" steps taken: " + String(steps_taken[i]));
     Serial.println();
     for(int i = 0; i<5; i++) send_message("MTR "+String(i)+" microseconds per step: " + String(time_steps[i]));
+    return true;
 }
 
+bool homing_command(){
+    struct VoltageAverage voltage_log[MTR_NUMBER];
+    for(int i=0; i<MTR_NUMBER; i++) voltage_log[i] = createVoltageAverage();
+
+    bool mtr_running [5] = {true, true, true, true, true};
+    //for(int i = 0; i<5; i++) setDirection(i,HOME_DIRECTION[i]);
+
+    int mtrs_done = 0;
+    for(int k = 0; k<5000; k++) {
+      for(int i = 0; i<5; i++) {
+        if(mtr_running[i]) {
+          //step(i);
+          mtr_running[i] = pushRollingAverage(random(70), &voltage_log[i]);
+          if(mtr_running[i]==false) mtrs_done++;
+        }
+      }
+      if(mtrs_done==MTR_NUMBER) return true;
+    }
+    return false;
+}
+
+/*
 // Sends a pulse on the NXT/STEP pin to tell the driver to take
 // one step, and also delays to control the speed of the motor.
 void step(int sel)
@@ -81,3 +116,4 @@ void setDirection(int sel, bool dir)
   digitalWrite(amisDirPin[sel], dir);
   delayMicroseconds(1);
 }
+*/
--- arduino/firmware/printer_control.h
+++ arduino/firmware/printer_control.h
@@ -4,8 +4,9 @@
 #include "Arduino.h"
 #include "SPI.h"
 #include "AMIS30543.h"
+#include "stall_detection.h"
 
-///*
+/*
 const uint8_t amisDirPin[5] = {PB11, PB12, PB13, PB14, PB15};
 const uint8_t amisStepPin[5] = {PC5, PC6, PC7, PC8, PC9};
 const uint8_t amisSlaveSelect[5] = {PB3, PB4, PB5, PB6, PB7};
@@ -18,7 +19,8 @@ const float time_per_cm = 1000000*4; //  3 000 000 = 3 seconds
 //const int cm_step_amount = (int)((float)steps_per_rotation * 0.2875); // UNNEEDED NOW, working with roation angles
 //const int mm_step_amount = (int)((float)cm_step_amount / 10); // UNNEEDED NOW, working with roation angles
 
-void move_command(int[5], bool);
+bool move_command(int[5], bool);
+bool homing_command();
 void step(int);
 void setDirection(int, bool);
 #endif
