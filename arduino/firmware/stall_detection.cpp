b6460548fcac7735a4bb557b455fa1d21b99b244
Lewis0305 <lewiss4@oregonstate.edu>
2022-04-26 18:37:46 -0700

    Pushed Stall Detection Test Code

--- arduino/firmware/stall_detection.cpp
+++ arduino/firmware/stall_detection.cpp
@@ -2,3 +2,100 @@
 #include "usb_lib.h"
 #include "printer_control.h"
 #include "stall_detection.h"
+
+//#include <stdio.h>
+
+struct VoltageAverage createVoltageAverage(void) {
+    struct VoltageAverage VAvg;
+    VAvg.pos = 0;
+    VAvg.len = 0;
+    VAvg.sum = 0;
+    return VAvg;
+}
+
+struct MotorData createMotorData(void) {
+    struct MotorData data;
+    for(int i = 0; i < Motor_Number; i++) {
+        data.MotorAverages[i] = createVoltageAverage();
+    }
+    return data;
+}
+
+void startUp() {
+    int gains [] = {1,0,1,1,1};
+    set_SLA_gain(gains);
+    int transparency_off [] = {1,0,1,1,1};
+    set_SLA_transparency_off(transparency_off);
+    int transparency_on [] = {0,1,0,0,0};
+    set_SLA_transparency_on(transparency_on);
+}
+
+double pushRollingAverage(int volt, struct VoltageAverage* VAvg) {
+    VAvg->voltages[VAvg->pos] = volt;
+    VAvg->sum += volt;
+    VAvg->len++; VAvg->pos++;
+    if (VAvg->pos >= Array_Size)
+        VAvg->pos = 0;
+    if (VAvg->len > Array_Size) {
+        VAvg->len--;
+        VAvg->sum -= VAvg->voltages[VAvg->pos];
+    }
+    return (double) VAvg->sum / VAvg->len;
+}
+
+int runHomingSequence_c(int time, struct testPINS PINS, struct MotorData* MData){
+    double averages[Motor_Number];
+    int motors [Motor_Number] = {0};
+    int motor;
+    for (time = 0; time < 600; time++) {
+
+        for (motor = 0; motor < Motor_Number; motor++) {
+            averages[motor] = pushRollingAverage(read_analog(PINS.PIN[motor], time), &MData->MotorAverages[motor]);
+            if(averages[motor] < Stall_Line)
+                motors[motor] = 1;
+        }
+        int MSum = 0;
+        for (int i = 0; i < Motor_Number; i++)
+            MSum += motors[i];
+        if(MSum == 5) {
+            printf("\nAverage: [    HOMED,     HOMED,     HOMED,     HOMED,     HOMED, ]  Time: %d", time);
+            printf("\nHoming Complete.\n");
+            return 0;
+        }
+        if (time % Output_Time == 0) {
+            printf("\nAverage: [");
+            for(motor = 0; motor < Motor_Number; motor++) {
+                if (motors[motor] == 1)
+                    printf("    HOMED, ");
+                else
+                    printf("%f, ", averages[motor]);
+            }
+            printf("]  Time: %d", time);
+        }
+    }
+    printf("\nHoming was never completed.\n");
+    return 1;
+}
+
+int runPrintSequence_c(int time, int data_list [Data_Length]){
+    int id, motor;
+    double average;
+    int motors [] = {0};
+    struct MotorData PrintingData = createMotorData();
+
+    for (time = 0; time < 600; time++) {
+        average = pushRollingAverage(read_analog(data_list, time), &PrintingData.MotorAverages[0]);
+
+        if (average < Stall_Line) {
+            printf("%f\n", average);
+            return 1;
+        }
+
+        if (time % Output_Time == 0) {
+            printf("%f\n", average);
+        }
+    }
+    printf("No Stall.\n0\n\n");
+
+    return 0;
+}
--- arduino/firmware/stall_detection.h
+++ arduino/firmware/stall_detection.h
@@ -3,5 +3,29 @@
 
 #include "Arduino.h"
 
+#define Array_Size 20
+#define Motor_Number 5
+#define Output_Time 100
+#define Stall_Line 20
+
+struct testPINS {
+    int PIN [Motor_Number][Data_Length];
+};
+
+struct VoltageAverage {
+    int pos, len, sum;
+    int voltages[Array_Size];
+};
+
+struct MotorData {
+    struct VoltageAverage MotorAverages[Motor_Number];
+};
+
+struct VoltageAverage createVoltageAverage(void);
+struct MotorData createMotorData(void);
+void startUp();
+double pushRollingAverage(int, struct VoltageAverage*);
+int runHomingSequence_c(int, struct testPINS, struct MotorData*);
+int runPrintSequence_c(int, int [Data_Length]);
 
 #endif
