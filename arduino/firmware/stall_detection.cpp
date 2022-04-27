af920d8e8e6f19f75ba8be9d267fd1c6bf056385
Lewis0305 <lewiss4@oregonstate.edu>
2022-04-26 17:12:09 -0700

    Added Stall Detection Files

---
+++ arduino/firmware/stall_detection.cpp
@@ -0,0 +1,4 @@
+#include "Arduino.h"
+#include "usb_lib.h"
+#include "printer_control.h"
+#include "stall_detection.h"
---
+++ arduino/firmware/stall_detection.h
@@ -0,0 +1,7 @@
+#ifndef STALL
+#define STALL
+
+#include "Arduino.h"
+
+
+#endif
