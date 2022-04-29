d3df2fa6838fed50e6a515ef72f0fa6d0ef3cb75
Lewis0305 <lewiss4@oregonstate.edu>
2022-04-29 15:02:16 -0700

    Updated stall detection method

--- arduino/firmware/printer_control.h
+++ arduino/firmware/printer_control.h
@@ -12,7 +12,7 @@ const uint8_t amisStepPin[5] = {PC5, PC6, PC7, PC8, PC9};
 const uint8_t amisSlaveSelect[5] = {PB3, PB4, PB5, PB6, PB7};
 const uint8_t amisSLA[5] = {PA0, PA1, PA4, PB0, PC1};
 //*/
-const float mvmt_time = 1000000*3; //  3 000 000 = 3 seconds
+const float mvmt_time = 1000000*1; //  3 000 000 = 3 seconds
 
 
 
--- arduino/firmware/stall_detection.cpp
+++ arduino/firmware/stall_detection.cpp
@@ -21,12 +21,11 @@ bool pushRollingAverage(int volt, struct VoltageAverage* VAvg) {
     VAvg->voltages[VAvg->pos] = volt;
     VAvg->sum += volt;
     VAvg->len++; VAvg->pos++;
-    if (VAvg->pos >= Array_Size)
-        VAvg->pos = 0;
+    if (VAvg->pos >= Array_Size) VAvg->pos = 0;
     if (VAvg->len > Array_Size) {
         VAvg->len--;
         VAvg->sum -= VAvg->voltages[VAvg->pos];
-        //if(volt < ((VAvg->sum / VAvg->len)/5)) return false; // testing new stall detection
+        //if(volt < ((VAvg->sum / VAvg->len)/2)) return false; // testing new stall detection
     }
     //Serial.println(String(VAvg->sum / VAvg->len));
     return true;
