1a307ffc7adcba3595511f65caee88097ea0c3a3
Lewis0305 <lewiss4@oregonstate.edu>
2022-05-03 13:11:37 -0700

    Commented firmware.ino

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,9 +1,24 @@
+//
 #include <SPI.h>
+
+// Motor Driver Library
 #include <AMIS30543.h>
+
+// USB Communication Function Library
 #include "usb_lib.h"
+
+// Printer Movement Function Library
 #include "printer_control.h"
+
+// Stall Detection Function Library
 #include "stall_detection.h"
+
+// Hot-End Control Function Library
 #include "hotend_control.hpp"
+
+/////////////////////////////////////////////////////////////////////
+
+// Requirement Grading:
 
 // Positional acc +/- 1deg && Arb Positioning
 // <!D(+0000.0000,+0000.0000,+0000.0000,-0000.7854,+0000.0000,t)>
@@ -27,19 +42,27 @@
 // Safety
 // visual inspection
 
-unsigned long redirectStart;
-char datatype;
+/////////////////////////////////////////////////////////////////////
 
+// Timer for how a serial event runs, blocking the main loop.
+unsigned long redirectStart;
+
+// Char holding the serial request command signifier.
+char signifier;
+
+// Array of Motor Driver Steppers.
 AMIS30543 stepper[5] = {};
 
 void setup()
 {
   SPI.begin();
   Serial.begin(9600);
+
+  // Shortens the timeout on readString() from 1000 to prevent
+  // long blocking.
   Serial.setTimeout(10);
 
-
-///*
+  // Setup for each motor driver
   for(int i = 0; i < 5; i++) {
     stepper[i].init(amisSlaveSelect[i]);
 
@@ -72,57 +95,90 @@ void setup()
     // Enable the motor outputs.
     stepper[i].enableDriver();
   }
-//*/
 
+  // Setup hotend with correct pinmodes and set variables.
   hotendSetup();
 }
 
 
 void loop()
 {
-    runHotend();
-    // Hotend Code?
-    //Serial.println(analogRead(amisSLA[0]));
+  // Maintain hot-end temp and check for thermal runaway.
+  runHotend();
+
+  // serialEvent is called by the arduino at the end of every cycle.
 } 
 
+// Handles any commands the printer recieves over serial.
 void serialEvent()
-{
-   // <!D(+1234.1234,-2345.2345,+3456.3456,-4567.4567,+5678.5678,t)><!e>
-   
-    while(Serial.available()) 
-    {
-        redirectStart = millis();
+{ 
+  // Checks if there is anything in the serial recieve buffer. The
+  // buffer holds 64 bytes.
+  while(Serial.available()) 
+  {
+    // Start the timer to record serial processing time.
+    redirectStart = millis();
 
-        String str = Serial.readString();
-        //Serial.println(str);
-        //Serial.println(str.substring(0,2));
-        //Serial.println(str.charAt(2));
-        //Serial.println(str.substring(4,60));
-        //Serial.println(str.substring(61,62));
-        //Serial.println(str.length());
+    // Read the entire buffer into a string
+    String serial_message = Serial.readString();
+
+    // Checks that the string from buffer is a serial command. i.e.
+    // <!{Command Signifier}{optional: (data)}>
+    // <!h>       - Homing Command
+    // <!n(200)>  - Set Hot-end to New Temp (200)
+    if(serial_message.substring(0,2) == "<!") {
+      
+      // Get command signifier char from buffer string.
+      signifier = serial_message.charAt(2); 
 
 
-        if(str.substring(0,2) == "<!") {
-            //send_message("GOOD COMMAND");
-            
-            datatype = str.charAt(2); 
+      // Re-direct to the command that was called.
 
-            //send_message("Recieved message in: " + String((double)(millis() - redirectStart)/1000.0) + " seconds");
+      // Signifier shows a move command and the move data is the right 
+      // length.
+      if (signifier == DATA && serial_message.substring(61,62) == ">") 
+        // Send the data to a data parser that will extract variables
+        // and call a move command.
+        handle_move(serial_message.substring(4,60));
 
-            if      (datatype == DATA && str.substring(61,62) == ">") parse_data(str.substring(4,60));
-            else if (datatype == HOMING_REQ) {
-              stepper[2].disableDriver();
-              homing_sequence();
-              stepper[2].enableDriver();
-            }
-            else if (datatype == FILAMENT) filament_sequence();
-            else if (datatype == TEMP_SET) hotendParse(str.substring(4));
-            else if (datatype == TEMP_REP) 
-              send_message("Current temp: " + String(hotin) + "°C, Target temp: " + String(hotset) + "°C");
-            else if (datatype == DEBUG) debug_mode();
-            else delay(100);
-        }
-        else send_message("BAD COMMAND");
-   }
-   send_message("Done in: " + String((double)(millis() - redirectStart)/1000.0) + " seconds");
+      // Signifier shows a homing request.
+      else if (signifier == HOMING_REQ) {
+        // Disable the z-axis motor so it will fall to the lowest point.
+        stepper[2].disableDriver();
+        
+        // Initiate a homing sequence.
+        handle_homing();
+
+        // Wait for the z-axis to fall and re-enable it.
+        //delay(1000*3); // 3sec delay to allow the z-axis to reach the bottom.
+        stepper[2].enableDriver();
+      }
+
+      // Signifier shows a filament extrusion test request.
+      else if (signifier == FILAMENT) 
+        // Initiate a filament test.
+        handle_filament_test();
+
+      // Signifier shows a hot-end tempature change request.
+      else if (signifier == TEMP_SET) 
+        // Call hot-end tempature change handler.
+        hotendParse(serial_message.substring(4));
+
+      // Signifier shows a hot-end status request.
+      else if (signifier == TEMP_REP) 
+        // Send message containing hot-end information.
+        send_message("Current temp: " + String(hotin) + "°C, Target temp: " + String(hotset) + "°C");
+      
+      // Signifer shows a debug mode request.
+      else if (signifier == DEBUG) 
+        // Call debug mode handler.
+        handle_debug();
+      
+      else send_message("UNKOWN COMMAND");;
+    }
+    else send_message("BAD COMMAND");
+  }
+
+  // Send message of the time it took to process buffer.
+  send_message("Done in: " + String((double)(millis() - redirectStart)/1000.0) + " seconds");
 }
