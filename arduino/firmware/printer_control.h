c44a384153c1edae5e19320cfd1c2a9a9cc088ca
Lewis0305 <lewiss4@oregonstate.edu>
2022-05-19 15:54:59 -0700

    Labeled more outdated legacy functions required to interface with IK

--- arduino/firmware/printer_control.h
+++ arduino/firmware/printer_control.h
@@ -13,7 +13,7 @@
 // Stall Detection Function Library
 #include "stall_detection.h"
 
-
+// PINS for Motors: ECE
 const uint8_t amisDirPin[5] = {PB11, PB12, PB13, PB14, PB15};
 const uint8_t amisStepPin[5] = {PC5, PC6, PC7, PC8, PC9};
 const uint8_t amisSlaveSelect[5] = {PB3, PB4, PB5, PB6, PB7};
@@ -29,12 +29,17 @@ const int min_mtr_delay = 200;
 // Pin replaced by extruder until board is fixed
 const int extruder_pin = 2;
 
+// move motors in accordance to old 5 motor IK data messages
 bool new_move_command(long[5], bool);
 
+// moves all 5 motors while extruding on a 6th at a set cm/s
 bool print_move_command(long[5], long);
 
-bool move_command(int[5], bool);
+// steps all motors at safe speed until all have stalled
 bool homing_command();
+
+// test function to allow for extruding at a cm/s (double) for
+// a set distance (int)
 bool extrude(double, int);
 
 // Sends a pulse on the NXT/STEP pin to tell the driver to take
