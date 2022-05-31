c47d00356340c10e18a8e24b2844687c225fc2d8
Lewis0305 <lewiss4@oregonstate.edu>
2022-05-30 23:28:48 -0700

    Removed Constants and Printer Move Function

--- arduino/firmware/printer_control.cpp
+++ arduino/firmware/printer_control.cpp
@@ -7,6 +7,9 @@
 // Motor Driver Library
 #include "AMIS30543.h"
 
+//
+#include "config.h"
+
 // USB Communication Function Library
 #include "usb_lib.h"
 
@@ -53,7 +56,7 @@ bool move_command(long stp_cnt[5], char ht_nd, long move_time){
   for(i = 0; i<5; i++){
     if(abs(stp_cnt[i]) > max) max = abs(stp_cnt[i]);
   }
-  unsigned long move_time = max*min_mtr_delay;
+  move_time = max*min_mtr_delay;
   for(i = 0; i<5; i++) {
     time_steps[i] = move_time / abs(stp_cnt[i]);
   }
@@ -102,7 +105,7 @@ bool move_command(long stp_cnt[5], char ht_nd, long move_time){
       }
     }
     // If the Extruder needs to step: step it an calculate next step time
-    if(ht_nd == 'e' && timeNow>=time_nxt_step_extruder){
+    if(ht_nd == 't' && timeNow>=time_nxt_step_extruder){
       step(extruder_pin);
       time_nxt_step_extruder += time_steps_extruder;
     }
@@ -114,123 +117,6 @@ bool move_command(long stp_cnt[5], char ht_nd, long move_time){
   return true;
 }
 
-
-bool print_move_command(long stp_cnt[5], long extrude_delay){
-  // timer for feedback on how long this function ran
-  unsigned long start_time = millis();
-
-  // Might not need to be initialized here
-  // Records the amount of steps taken
-  long steps_taken[5] = {0, 0, 0, 0, 0};
-
-  // Data for stall detection, may be moved to stall_detection.cpp
-  struct StallData voltage_log[MTR_NUMBER];
-  for(int i=0; i<MTR_NUMBER; i++) voltage_log[i] = createVoltageAverage();
-
-  // Set each motor in the correct direction
-  for(int i = 0; i<5; i++) {
-    if(stp_cnt[i] >= 0) setDirection(i,POS_DIRECTION[i]);
-    else setDirection(i,NEG_DIRECTION[i]);
-  }
-
-  // Time between steps on a specific motor
-  // Should add a case where: if a time_step[i] is less than 150, add
-  // time to mvmt_time and recalculate. This will avoid missing steps
-  long added_time = 0;
-  bool mtr_ready = false;
-  while(!mtr_ready) {
-    mtr_ready = true;
-    for(int i = 0; i<5; i++) {
-      if(stp_cnt[i] == 0) time_steps[i] = 0;
-      else{
-        time_steps[i] = (mvmt_time + added_time) / abs(stp_cnt[i]);
-        if(time_steps[i]<min_mtr_delay) {
-          added_time += 5000; // half sec
-          mtr_ready = false;
-        }
-      }
-    }
-  }
-
-
-  // Set extrusion to correct direction
-  if (extrude_delay>0)setDirection(extruder_pin,0);
-  else setDirection(extruder_pin,1);
-  // Set motor step delay
-  long time_steps_extruder = abs(extrude_delay);
-
-
-  // Timer that the motors will trigger off
-  unsigned long timeBegin = micros();
-  
-  // Time at which the next step should be taken for each motor
-  float time_nxt_step [5] = {0, 0, 0, 0, 0};
-  // Calculate the time of first step for each motor
-  for(int i = 0; i<5; i++) time_nxt_step[i] += timeBegin + time_steps[i];
-  float time_nxt_step_extruder = timeBegin + time_steps_extruder;
-  
-
-  send_message("Starting move command");
-
-  // SLA Log for testing
-  //signed int list_sla [abs(stp_cnt[1])] = {0};
-
-  int i;
-  // Record current time for stepping
-  unsigned long timeNow = micros();
-
-  // Set all motors to not done
-  bool done_mtrs [5] = {false};
-  int motors_done = 0;
-  // End when all motors are done
-  while(motors_done < 5){
-    timeNow = micros();
-
-    // Step on each of the joint motors if needed
-    delayMicroseconds(200);
-    for(i = 0; i<5; i++){
-      
-      // If motor is done don't step it
-      if(done_mtrs[i]==true) {}
-      // If the if now finishing make it as done
-      else if(steps_taken[i]==abs(stp_cnt[i])){
-        //Serial.println("Done: Motor " + String(i));
-        done_mtrs[i] = true;
-        motors_done++;
-      // If the motor is ready to step
-      } else if(timeNow>=time_nxt_step[i]){
-        step(i);
-        
-        //if(i==1 && abs(steps_taken[i])%1==0) list_sla[abs(steps_taken[i])] = analogRead(amisSLA[i]); // mtr_ready = false;//
-
-        // Stall detection check
-        //maybe remove abs from modulo
-        //if(abs(steps_taken[i])%Stall_Check_Step[i]==0 && pushVoltage(i, &voltage_log[i]) == false) {
-        //    stop_message("Stall on motor " + String(i));
-        //    return false;
-        //}
-
-        // Calculate new step trigger time
-        time_nxt_step[i] += time_steps[i];
-        steps_taken[i]++;
-      }
-    }
-    // If the Extruder needs to step: step it an calculate next step time
-    if(timeNow>=time_nxt_step_extruder && extrude_delay != 0){
-      step(extruder_pin);
-      time_nxt_step_extruder += time_steps_extruder;
-    }
-  }
-
-  // Log gathered SLA data
-  //for(int i=0; i<abs(stp_cnt[1]); i++) Serial.println(list_sla[i]);
-  
-  // Log information about movement for debugging
-  send_message("Done in "+String((double)(millis()-start_time)/1000.0)+" seconds");
-  for(int i = 0; i<5; i++) send_message("MTR "+String(i)+" steps taken: " + String(steps_taken[i]));
-  for(int i = 0; i<5; i++) send_message("MTR "+String(i)+" microseconds per step: " + String(time_steps[i]));
-  return true;
-}
 
 bool homing_command(){
     //return true;
